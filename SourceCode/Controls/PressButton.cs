69d8a33ded5f39570a97a0231707cf2b7e0fc677
devgis <devgis@163.com>
2021-06-25 09:49:59 +0800

    V0
    V0

---
+++ SourceCode/Controls/PressButton.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DEVGIS.Controls
+{
+    public class PressButton:Button
+    {
+        public Color FocusedColor
+        {
+            get; set;
+        } = Color.FromArgb(255, 150, 150, 150);
+
+        public Color NotFocusedColor
+        {
+            get; set;
+        } = Color.FromArgb(255,220, 220, 220);
+
+        public Color MouseOverColor
+        {
+            get; set;
+        } = Color.Orange;
+        public PressButton() : base()
+        {
+            //this.FlatAppearance.BorderSize = 0;
+            //this.FlatAppearance.MouseDownBackColor = Color.DarkOrange;
+            //this.FlatAppearance.MouseOverBackColor = MouseOverColor;
+            //this.MouseOverColor = MouseOverColor;
+        }
+
+        protected override void OnCreateControl()
+        {
+            base.OnCreateControl();
+            this.BackColor = NotFocusedColor;
+            this.FlatStyle = FlatStyle.Standard;
+            this.Refresh();
+            
+        }
+
+        protected override void OnGotFocus(EventArgs e)
+        {
+            base.OnGotFocus(e);
+            this.FlatStyle = FlatStyle.Flat;
+            this.BackColor = FocusedColor;
+        }
+
+        protected override void OnLostFocus(EventArgs e)
+        {
+            base.OnLostFocus(e);
+            this.FlatStyle = FlatStyle.Standard;
+            this.BackColor = NotFocusedColor;
+        }
+    }
+}
---
+++ SourceCode/TestPressButton/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿
+namespace TestPressButton
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pressButton4 = new DEVGIS.Controls.PressButton();
+            this.pressButton3 = new DEVGIS.Controls.PressButton();
+            this.pressButton2 = new DEVGIS.Controls.PressButton();
+            this.pressButton1 = new DEVGIS.Controls.PressButton();
+            this.pressButton5 = new DEVGIS.Controls.PressButton();
+            this.pressButton6 = new DEVGIS.Controls.PressButton();
+            this.pressButton7 = new DEVGIS.Controls.PressButton();
+            this.pressButton8 = new DEVGIS.Controls.PressButton();
+            this.SuspendLayout();
+            // 
+            // pressButton4
+            // 
+            this.pressButton4.Location = new System.Drawing.Point(627, 37);
+            this.pressButton4.Name = "pressButton4";
+            this.pressButton4.Size = new System.Drawing.Size(184, 57);
+            this.pressButton4.TabIndex = 3;
+            this.pressButton4.Text = "pressButton4";
+            this.pressButton4.UseVisualStyleBackColor = true;
+            // 
+            // pressButton3
+            // 
+            this.pressButton3.Location = new System.Drawing.Point(425, 37);
+            this.pressButton3.Name = "pressButton3";
+            this.pressButton3.Size = new System.Drawing.Size(184, 57);
+            this.pressButton3.TabIndex = 2;
+            this.pressButton3.Text = "pressButton3";
+            this.pressButton3.UseVisualStyleBackColor = true;
+            // 
+            // pressButton2
+            // 
+            this.pressButton2.Location = new System.Drawing.Point(226, 37);
+            this.pressButton2.Name = "pressButton2";
+            this.pressButton2.Size = new System.Drawing.Size(184, 57);
+            this.pressButton2.TabIndex = 1;
+            this.pressButton2.Text = "pressButton2";
+            this.pressButton2.UseVisualStyleBackColor = true;
+            // 
+            // pressButton1
+            // 
+            this.pressButton1.Location = new System.Drawing.Point(24, 37);
+            this.pressButton1.Name = "pressButton1";
+            this.pressButton1.Size = new System.Drawing.Size(184, 57);
+            this.pressButton1.TabIndex = 0;
+            this.pressButton1.Text = "pressButton1";
+            this.pressButton1.UseVisualStyleBackColor = true;
+            // 
+            // pressButton5
+            // 
+            this.pressButton5.Location = new System.Drawing.Point(627, 115);
+            this.pressButton5.Name = "pressButton5";
+            this.pressButton5.Size = new System.Drawing.Size(184, 57);
+            this.pressButton5.TabIndex = 7;
+            this.pressButton5.Text = "pressButton5";
+            this.pressButton5.UseVisualStyleBackColor = true;
+            // 
+            // pressButton6
+            // 
+            this.pressButton6.Location = new System.Drawing.Point(425, 115);
+            this.pressButton6.Name = "pressButton6";
+            this.pressButton6.Size = new System.Drawing.Size(184, 57);
+            this.pressButton6.TabIndex = 6;
+            this.pressButton6.Text = "pressButton6";
+            this.pressButton6.UseVisualStyleBackColor = true;
+            // 
+            // pressButton7
+            // 
+            this.pressButton7.Location = new System.Drawing.Point(226, 115);
+            this.pressButton7.Name = "pressButton7";
+            this.pressButton7.Size = new System.Drawing.Size(184, 57);
+            this.pressButton7.TabIndex = 5;
+            this.pressButton7.Text = "pressButton7";
+            this.pressButton7.UseVisualStyleBackColor = true;
+            // 
+            // pressButton8
+            // 
+            this.pressButton8.Location = new System.Drawing.Point(24, 115);
+            this.pressButton8.Name = "pressButton8";
+            this.pressButton8.Size = new System.Drawing.Size(184, 57);
+            this.pressButton8.TabIndex = 4;
+            this.pressButton8.Text = "pressButton8";
+            this.pressButton8.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 450);
+            this.Controls.Add(this.pressButton5);
+            this.Controls.Add(this.pressButton6);
+            this.Controls.Add(this.pressButton7);
+            this.Controls.Add(this.pressButton8);
+            this.Controls.Add(this.pressButton4);
+            this.Controls.Add(this.pressButton3);
+            this.Controls.Add(this.pressButton2);
+            this.Controls.Add(this.pressButton1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DEVGIS.Controls.PressButton pressButton1;
+        private DEVGIS.Controls.PressButton pressButton2;
+        private DEVGIS.Controls.PressButton pressButton3;
+        private DEVGIS.Controls.PressButton pressButton4;
+        private DEVGIS.Controls.PressButton pressButton5;
+        private DEVGIS.Controls.PressButton pressButton6;
+        private DEVGIS.Controls.PressButton pressButton7;
+        private DEVGIS.Controls.PressButton pressButton8;
+    }
+}
+
